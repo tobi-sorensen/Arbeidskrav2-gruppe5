913b835380f5bd1db851106a583095036eb7d96d
S8N <92916913+tobi-sorensen@users.noreply.github.com>
2025-03-27 17:17:04 +0100

    Gjort ferdig crud loaklt

--- apilocalcrud.js
+++ apilocalcrud.js
@@ -1,5 +1,5 @@
 // Felles API, LocalStorage, CRUD
-
+const axios = require("axios");
 // Henter ALL data fra SWAPI med 
 async function fetchAllData(apiUrl) {
     let results = [];
@@ -31,3 +31,55 @@ async function fetchAllData(apiUrl) {
   function getFromLocalStorage(key) {
     return JSON.parse(localStorage.getItem(key)) || [];
   }
+
+// CRUD for character
+const apiKeyCharacter = "tR1Qe84J9e05tUfCBgJyOmIe_A3k7vvDHubu0ZqyulccvGsXtQ"
+const apiUrlCharacter = "https://crudapi.co.uk/api/v1/character"
+
+const newCharacter = [
+  {
+    name: "Luke Skywalker",
+    birthYear: "19 bby",
+    movies: "456789",
+    species: "Human"
+  },
+];
+
+async function addCharacter(characterData, apiKeyCharacter) {
+  try {
+    const response = await axios.post(apiUrlCharacter, characterData, {
+      headers: { Authorization: `Bearer ${apiKeyCharacter}`}
+    });
+    console.log("Ny karakter lagt til", response.data);
+  } catch (error) {
+    console.error("Klarte ikke å legge til karkater", error);
+  }
+};
+
+addCharacter(newCharacter, apiKeyCharacter);
+
+// CRUD for vehicles
+const apiKeyVehicles = "dHuEQ2iFrA4GCXpAInw8QO3OdsEFCqWH2sjpeIKwQ1dCj_J3zA"
+const apiUrlVehicles = "https://crudapi.co.uk/api/v1/vehicles"
+
+const newVehicle = [
+  {
+    name: "Sand crawler",
+    model: "Digger crawler",
+    cargoCapacity: "50000",
+    cost: "150000"
+  }
+]
+
+async function addVehicle(vehicleData, apiKeyVehicles) {
+  try {
+    const response = await axios.post(apiUrlVehicles, vehicleData, {
+      headers: {Authorization: `Bearer ${apiKeyVehicles}`}
+    });
+    console.log("Nytt kjøretøy lagt til", response.data);
+  } catch (error) {
+    console.error("Klarte ikke å legge til kjøretøy", error)
+  }
+}
+
+addVehicle(newVehicle, apiKeyVehicles);
